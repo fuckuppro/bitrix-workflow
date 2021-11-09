4959304dc550dba302b732e5f273100fd2dc6973
fuckuppro <mostovoi@bk.ru>
2021-11-09 23:31:40 +0300

    Код выполнения
    Код выполнения команды

--- command_task.php
+++ command_task.php
@@ -1,2 +1,16 @@
 <?php
 // пример выполнения команды
+global $USER;
+$arCurrentUserGroups = $USER->GetUserGroupArray();
+$sWorkFlowId = '';
+$sWorkFlowEvent = 'event_name'; // activity code
+$arWorkFlowEventParams = [];
+$arWorkFlowEventErrors = [
+    "Groups" => $arCurrentUserGroups, "User" => $USER->GetID()
+];
+CBPDocument::SendExternalEvent(
+    $sWorkFlowId,
+    $sWorkFlowEvent,
+    $arWorkFlowEventParams,
+    $arWorkFlowEventErrors
+);
